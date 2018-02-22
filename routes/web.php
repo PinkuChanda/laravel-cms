db49c84ea99ba6c3bdcc998676053bbf6301161d
pinku chanda <pinkuchanda654@gmail.com>
2018-02-23 00:55:17 +0600

    Node module install with Sass

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,20 @@
 |
 */
 
+use App\User;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin', function(){
+	return view('admin.index');
+});
+
+// Admin Users Controller  
+
+Route::resource('admin/users', 'AdminUserController');
